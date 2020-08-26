b41b1d0d5b17ac08fafd407ec3032bff5b4059f6
nanderton <noelanderton@cox.net>
2020-08-26 09:54:44 -0700

    clean up add TestCommand as seperate file

--- Wreb.IntegrationTests/BinarySerializerTests.cs
+++ Wreb.IntegrationTests/BinarySerializerTests.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Wreb.Integration;
-using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -20,30 +19,6 @@ namespace Wreb.Integration.Tests
 
             TestCommand actual = (TestCommand)BinarySerializer.Deserialize(BinarySerializer.Serialize(testCommand));
             Assert.AreEqual(testCommand, actual);
-        }
-    }
-
-    [Serializable]
-    public class TestCommand : Command, ICommand
-    {
-        public TestCommand(string originUser, string originSystem, string commandAction, string connectionId, int? id, string clientId) : 
-            base(originUser, originSystem, commandAction, connectionId, id, clientId)
-        {
-
-        }
-
-        public string TestProperty { get; set; }
-
-        public override bool Equals(object obj)
-        {
-            return obj is TestCommand command &&
-                   TestProperty == command.TestProperty &&
-                   base.Equals(command);
-        }
-
-        public override int GetHashCode()
-        {
-            return base.GetHashCode();
         }
     }
 }
---
+++ Wreb.IntegrationTests/TestCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Wreb.Integration.Tests
+{
+    [Serializable]
+    public class TestCommand : Command, ICommand
+    {
+        public TestCommand(string originUser, string originSystem, string commandAction, string connectionId, int? id, string clientId) : 
+            base(originUser, originSystem, commandAction, connectionId, id, clientId)
+        {
+
+        }
+
+        public string TestProperty { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is TestCommand command &&
+                   TestProperty == command.TestProperty &&
+                   base.Equals(command);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
